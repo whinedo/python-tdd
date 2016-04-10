afb47f4e0ff0b057db79b69e39936bfe96d0bc9a
hector <whinedo@gmail.com>
2016-04-10 11:49:21 +0200

    refactored code in views to be in order to Django v+10

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
+import lists.views
 
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    url(r'^$', 'lists.views.home_page', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+#urlpatterns = patterns('',
+#    # Examples:
+#    url(r'^$', 'lists.views.home_page', name='home'),
+#    # url(r'^blog/', include('blog.urls')),
+#
+#    # url(r'^admin/', include(admin.site.urls)),
+#)
 
-    # url(r'^admin/', include(admin.site.urls)),
-)
+
+urlpatterns = [
+    #url(r'^$', 'lists.views.home_page', name='home'), # old way for defining a url pattern
+    url(r'^$', lists.views.home_page, name='home'),
+]
