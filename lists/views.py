84ffbd70f55ef0a923f816fe407fa47799a9cb0b
hector <whinedo@gmail.com>
2016-04-22 21:53:27 +0200

    new URL + view for adding to existing lists. FT passes :-)

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 
@@ -13,11 +13,18 @@ def home_page(request):
 	return render(request, 'home.html')
 
 @csrf_exempt
-def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html',{'items':items})
+def view_list(request,list_id):
+	list_ = List.objects.get(id=list_id)
+	return render(request, 'list.html',{'list':list_})
 
 @csrf_exempt
 def new_list(request):
-	Item.objects.create(text=request.POST['item_text'])
-	return redirect('/lists/the-only-list-in-the-world/')
+	list_ = List.objects.create()
+	Item.objects.create(text=request.POST['item_text'],list=list_)
+	return redirect('/lists/%d/'%(list_.id,))
+
+@csrf_exempt
+def add_item(request,list_id):
+	list_ = List.objects.get(id=list_id)
+	Item.objects.create(text=request.POST['item_text'],list=list_)
+	return redirect('/lists/%d/'%(list_.id,))
