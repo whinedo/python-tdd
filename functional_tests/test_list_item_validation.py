ff5def84130e1e098b32ee95454970529e5a5ceb
hector <whinedo@gmail.com>
2016-05-01 22:11:37 +0200

    use form in all views, back to working state

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,7 +8,7 @@ class ItemValidationTest(FunctionalTest):
 		# an empty list item. She hits Enter on the empty input box
 		self.browser.get(self.server_url)
 		inputbox = self.get_item_input_box()
-		inputbox().send_keys('\n')
+		inputbox.send_keys('\n')
 	
 		# The home page refreshes, and there is an error message saying
 		# that list items cannot be blank
@@ -22,7 +22,8 @@ class ItemValidationTest(FunctionalTest):
 		self.check_for_row_in_list_table('1: Buy milk')
 
 		# Perversely, she now decides to submit a second blank list item
-		self.browser.find_element_by_id('id_new_item').send_keys('\n')
+		inputbox = self.get_item_input_box()
+		inputbox.send_keys('\n')
 
 		# She receives a similar warning on the list page
 		self.check_for_row_in_list_table('1: Buy milk')
