ae18ebbfab5a950e771d5d2c3b34c232a36712c7
hector <whinedo@gmail.com>
2016-05-01 21:24:35 +0200

    use new form in home_page, simplify tests. NB breaks stuff

--- lists/views.py
+++ lists/views.py
@@ -3,15 +3,13 @@ from django.core.exceptions import ValidationError
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from lists.models import Item, List
+from lists.forms import ItemForm
 
 # Create your views here.
 
 @csrf_exempt
 def home_page(request):
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
-	return render(request, 'home.html')
+	return render(request, 'home.html',{'form':ItemForm()})
 
 @csrf_exempt
 def view_list(request,list_id):
@@ -20,20 +18,17 @@ def view_list(request,list_id):
 
 	if request.method == 'POST':
 		try:
-		#	item = Item.objects.create(text=request.POST['item_text'],list=list_)
 			item = Item(text=request.POST['item_text'], list=list_)
 			item.full_clean()
 			item.save()
 			return redirect(list_)
 		except ValidationError,e:
-			#list_.delete()
 			error = "You can't have an empty list item"
 	return render(request, 'list.html',{'list':list_,'error':error})
 
 @csrf_exempt
 def new_list(request):
 	list_ = List.objects.create()
-#	item = Item.objects.create(text=request.POST['item_text'],list=list_)
 	item = Item(text=request.POST['item_text'], list=list_)
 
 	try:
